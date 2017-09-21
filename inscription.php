9a21b3af73c5605fb0ea877a29bfa7ad951ec16b
Julie.Canevet <julie.canevet@hotmail.fr>
2017-09-21 10:33:09 +0200

    first commit

---
+++ inscription.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Inscription</title>
+	</head>
+	<body>
+		<form action="inscription_post.php" method="post">
+			<input type="varchar" name="pseudo">
+			<input type="password" name="pass">
+			<input type="submit">
+		</form>
+	</body>
+</html>
